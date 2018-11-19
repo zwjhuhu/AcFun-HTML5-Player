51acd0f3498b041aaa4ee15e1ac4d70eec929f05
wenjun.zhou <zwjhuhu@126.com>
2018-11-19 17:32:05 +0800

    give a navigator.sendBeacon replace for log in beforeunload and pagehide event

--- src/analytic_fixer.js
+++ src/analytic_fixer.js
@@ -1,8 +1,27 @@
 let script = document.createElement('script');
-//just disable sync xhr
+// just disable sync xhr before document complete
+// but give a navigator.sendBeacon replace in beforeunload and pagehide event
 script.textContent = `
-  const oriOpen = window.XMLHttpRequest.prototype.open;
-  window.XMLHttpRequest.prototype.open = function(method,url,async){
-    oriOpen.call(this,method,url);
-  }`;
+  (function(){
+      const oriOpen = window.XMLHttpRequest.prototype.open;
+      window.XMLHttpRequest.prototype.open = function(method,url,async){
+        if (async === false) {
+            console.log('requested sync xhr to: ', url);
+        }
+        oriOpen.call(this,method,url);
+      }
+      window.addEventListener('beforeunload', function () {
+        window.XMLHttpRequest = function () {
+          return {
+            open: function (method, url, async) {
+              this.url = url;
+            },
+            send: function (data){
+              navigator.sendBeacon(this.url, data);
+              console.log('beacon queued', this.url, data);
+            },
+          }
+        };
+      });
+  })()`;
 document.firstElementChild.appendChild(script);
